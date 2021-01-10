28829af1b84a0462b7dc0e9325814eee16ad16a8
s3783258 <s3783258@student.rmit.edu.au>
2021-01-10 18:02:26 +1100

    Add project files.

---
+++ s3783258_a1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace s3783258_a1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
