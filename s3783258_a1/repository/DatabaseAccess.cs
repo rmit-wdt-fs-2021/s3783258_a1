677f6e324c59e555ec4fb430bc1149558b334f3f
s3783258 <s3783258@student.rmit.edu.au>
2021-01-12 12:17:42 +1100

    Store json in list from webservice

---
+++ s3783258_a1/repository/DatabaseAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s3783258_a1.repository
+{
+    class DatabaseAccess
+    {
+        
+    }
+}
