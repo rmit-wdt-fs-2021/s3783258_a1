c0167ae040db69b68b4ae7b5ad01c1c798d0b2ab
s3783258 <s3783258@student.rmit.edu.au>
2021-01-12 17:01:11 +1100

    Create Tables from SQL File

--- s3783258_a1/repository/DatabaseAccess.cs
+++ s3783258_a1/repository/DatabaseAccess.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace s3783258_a1.repository
 {
     class DatabaseAccess
     {
-        
+        public void CreateTables(string connectionString)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = File.ReadAllText("sql/CreateTables.sql");
+
+            command.ExecuteNonQuery();
+        }
     }
 }
