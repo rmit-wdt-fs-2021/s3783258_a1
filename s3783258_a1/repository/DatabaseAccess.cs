1dd1c06e87f8732c79316edb440298aa14e833dc
s3783258 <s3783258@student.rmit.edu.au>
2021-01-13 12:23:54 +1100

    Added check for row count

--- s3783258_a1/repository/DatabaseAccess.cs
+++ s3783258_a1/repository/DatabaseAccess.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data;
 
 namespace s3783258_a1.repository
 {
     class DatabaseAccess
     {
         //Creates the tables in the database given in the connection string
-        public void CreateTables(string connectionString)
+        public bool CreateTables(string connectionString)
         {
             using var connection = new SqlConnection(connectionString);
             connection.Open();
@@ -23,10 +24,35 @@ namespace s3783258_a1.repository
             try
             {
                 command.ExecuteNonQuery();
+                return true;
             } catch (SqlException e)
             {
-                Console.WriteLine("Error: Tables Already Exist");
+                return false;
             }
         }
+
+        //Checks to see if there are any rows in any table.
+        public bool DataCount(string connectionString)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var command = new SqlCommand("DataCount", connection) { CommandType = CommandType.StoredProcedure };
+            int count = (int)command.ExecuteScalar();
+            
+            if (count > 0)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        public void PopulateDatabase()
+        {
+            
+        }
+
     }
 }
